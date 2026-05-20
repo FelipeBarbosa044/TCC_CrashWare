c0b0ab8b0b69f1cd061cd84ec951fba58fa802b5
Gabriel Costa <costasousagabriel075@gmail.com>
2026-05-20 15:04:26 -0300

    Davi continua

--- frontend/Site/CrashWare/src/Paginas/ADM/AbasAdm/AddConquistas/AbaConquistas.jsx
+++ frontend/Site/CrashWare/src/Paginas/ADM/AbasAdm/AddConquistas/AbaConquistas.jsx
@@ -49,97 +49,44 @@ const AbaConquistas = () => {
                 />
             )}
 
-
             <div className={Style.Conteudos}>
-                <div className={Style.ConquistaInputs}>
-                    <div className={Style.campoForm}>
+                <div className={Style.NomeTipo}>
 
-                        <label htmlFor="NomeConsquista" className={Style.Margincima}>Nome da Conquista</label>
-                        <CampoTexto
-                            placeholder="Nome da Conquista"
-                            maxlenght={100}
-                            onChange={(e) => setNomeConquista(e.target.value)}
-                        />
-                        <p>Máx 100 caracteres</p>
-                    </div>
+                    {/* Nome Conquista */}
+                    <label htmlFor="NomeConsquista" className={Style.Margincima}>Nome da Conquista</label>
+                    <CampoTexto
+                        placeholder="Nome da Conquista"
+                        maxlenght={100}
+                        onChange={(e) => setNomeConquista(e.target.value)}
+                    />
+                    <p>Máx 100 caracteres</p>
 
-                    {/* <div className={Style.ConquistaInputs}> */}
-                    <div className={Style.campoFormDescricao}>
-                        <label htmlFor="Descricao">Descricao</label>
-                        <textarea
-                            name="" id="" className={Style.Descricao}
-                            placeholder="Insira uma breve descrição aqui"
-                            maxLength={300}
-                            onChange={(e) => setDescricaoConquista(e.target.value)}
-                        ></textarea>
-                        <p>Máx 300 caracteres</p>
-                    </div>
-
-                    <div className={Style.campoFormDescricao}>
-                        <label htmlFor="Condicao">Condição</label>
-                        <textarea
-                            name="" id="" className={Style.Descricao}
-                            placeholder="Coloque aqui as condições para desbloquear a conquista"
-                            maxLength={300}
-                            onChange={(e) => setCondicao(e.target.value)}
-                        ></textarea>
-                        <p>Máx 300 caracteres</p>
-                    </div>
-                </div>
-
-
-                {/* <div className={Style.ConquistaAtributos}> */}
-                <div className={Style.campoFormAtribustos}>
+                    {/* Tipo da Conquista */}
                     <h3>Tipo</h3>
-                    <label className={Style.RadioTipo}>
-                        <CampoTexto name="opcao" value="Software" type="radio"
-                            onChange={(e) => setOpcao(e.target.value)}
-                        />
-                        <img src={ImgSoftware} alt="Software" /> Software
-                    </label>
-
-                    <label className={Style.RadioTipo}>
-                        <CampoTexto name="opcao" value="Hardware" type="radio"
-                            onChange={(e) => setOpcao(e.target.value)}
-                        />
-                        <img src={ImgHardware} alt="Hardware" /> Hardware
-                    </label>
-                    <label className={Style.RadioTipo}>
-                        <CampoTexto name="opcao" value="Outro" type="radio"
-                            onChange={(e) => setOpcao(e.target.value)}
-                        />
-                        Outro
-                    </label>
-                    <div className={Style.Recompensas}>
-                        <h3>Recompensas</h3>
-
-                        <label htmlFor="moedas">Qntd de Moedas</label>
-                        <CampoTexto
-                            className={Style.Atributos}
-                            placeholder="Quantidade de Moedas"
-                            type="number"
-                            onChange={(e) => setMoedas(e.target.value)}
-                        />
-
-                        <label htmlFor="xp">Quantidade de XP</label>
-                        <CampoTexto
-                            className={Style.Atributos}
-                            placeholder="Quantidade de XP"
-                            type="number"
-                            onChange={(e) => setXP(e.target.value)}
-                        />
-                    </div>
-                                    <BotoesForm
-                    className={Style.botaoAdicionar}
-                    texto="Adicionar"
-                    disabled={!botaoliberado}
-                    onClick={handleAdicionarConquista}
-                />
-
-                </div>
-
-            </div>
             
+                    <div className={Style.CaixaRadio}>
+                        <label className={Style.RadioTipo}>
+                            <CampoTexto name="opcao" value="Software" type="radio"
+                                onChange={(e) => setOpcao(e.target.value)}
+                            />
+                            <img src={ImgSoftware} alt="Software" /> Software
+                        </label>
+
+                        <label className={Style.RadioTipo}>
+                            <CampoTexto name="opcao" value="Hardware" type="radio"
+                                onChange={(e) => setOpcao(e.target.value)}
+                            />
+                            <img src={ImgHardware} alt="Hardware" /> Hardware
+                        </label>
+                        <label className={Style.RadioTipo}>
+                            <CampoTexto name="opcao" value="Outro" type="radio"
+                                onChange={(e) => setOpcao(e.target.value)}
+                            />
+                            Outro
+                        </label>
+                    </div> {/* Tipo */}
+                </div> {/* Nome e Tipo */}
+            </div> {/* Conteudo */}
         </>
     )
 }
