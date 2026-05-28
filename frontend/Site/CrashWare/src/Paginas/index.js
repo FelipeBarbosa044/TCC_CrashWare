7ef9bebf6e442cc05e087c6f5606a235340418a8
Gabriel Costa <costasousagabriel075@gmail.com>
2026-05-28 14:19:23 -0300

    Layout verificar tel

--- frontend/Site/CrashWare/src/Paginas/index.js
+++ frontend/Site/CrashWare/src/Paginas/index.js
@@ -3,20 +3,20 @@ export * from './AlterarSenha';
 export * from './Anotacoes';
 export * from './cadastro';
 export * from './Configuracoes';
+export * from './ConteudoHardware';
+export * from './ConteudoSoftware';
 export * from './Home';
 export * from './Login';
+export * from './Loja';
 export * from './Perfil';
 export * from './PgErro';
 export * from './Principal';
 export * from './RecuperarSenha';
+export * from './Sobre';
 export * from './SobreNos';
 export * from './Termos';
 export * from './VerificacaoEmail';
-export * from './ConteudoHardware';
-export * from './ConteudoSoftware';
-export * from './Loja';
-export * from './Sobre';
+export * from './VerificarTelefone'
 
 //     Aulas
-
 export * from "./Aulas";
