f1a624093770aae9cde8e90eee1381c8d0281efc
Gabriel Costa <costasousagabriel075@gmail.com>
2026-05-30 16:21:31 -0300

    Aba Usuarios Layout

--- frontend/Site/CrashWare/src/Paginas/ADM/AbasAdm/AbaUsuarios/AbaUsuarios.jsx
+++ frontend/Site/CrashWare/src/Paginas/ADM/AbasAdm/AbaUsuarios/AbaUsuarios.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { CampoTexto } from '../../../../Componentes'
 import { BotoesForm } from '../../../../Componentes'
+import Defaut from '../../../../fotos/FotoPerfilPadrao.jpeg'
 
 import Style from './AbaUsuarios.module.css'
 
@@ -11,10 +12,17 @@ const AbaUsuarios = () => {
     // const Nome = usuario?.nome;
 
     const CONQUISTAS_MOCK = [
-        { id: 1, nome: 'Usuario', adm: 'Sim', deletar: 'Deletar' },
+        { id: 1, foto: Defaut, nome: 'UserName', nivel: "XX", criado: "00/00/0000", editar: 'Editar' },
+        { id: 2, foto: Defaut, nome: 'UserName', nivel: "XX", criado: "00/00/0000", editar: 'Editar' },
+        { id: 3, foto: Defaut, nome: 'UserName', nivel: "XX", criado: "00/00/0000", editar: 'Editar' },
+        { id: 4, foto: Defaut, nome: 'UserName', nivel: "XX", criado: "00/00/0000", editar: 'Editar' },
+        { id: 5, foto: Defaut, nome: 'UserName', nivel: "XX", criado: "00/00/0000", editar: 'Editar' },
+        
     ]
 
     const [conquistas, setConquistas] = useState(CONQUISTAS_MOCK);
+    const [fechado, setFechado] = useState(null);
+
     return (
         <>
             <div className={Style.separarConteudos}>
@@ -41,19 +49,48 @@ const AbaUsuarios = () => {
                             >
 
                                 <div className={Style.ItensLista}>
-                                    <h6>{c.nome}</h6>
-                                    {/* <p>Adm? {c.adm}</p> */}
-                                    <div className={Style.Botoes}>
-                                        <BotoesForm
-                                            texto="Editar"
-                                        />
-                                        <BotoesForm
-                                            className={Style.botaoDeletar}
-                                            texto={c.deletar}
-                                        />
+                                    <img src={c.foto} alt="Foto de Perfil" />
+                                    <div className={Style.Dados}>
+                                        <h6>{c.nome}</h6>
+                                        <p>Cadastrado em: {c.criado}</p>
                                     </div>
 
+                                    <p>Nível: {c.nivel}</p>
+
+                                    <div className={Style.Botoes}>
+                                        <BotoesForm
+                                            texto="Ver Perfil"
+                                        />
+                                        <BotoesForm
+                                            onClick={() => setFechado(fechado === c.id ? null : c.id)}
+                                            texto={c.editar}
+                                        />
+                                    </div>
                                 </div>
+                                {fechado === c.id && (
+                                    <div className={Style.sanfona}>
+                                        <BotoesForm
+                                            className={Style.btnSanfona}
+                                            texto="Remover Foto"
+                                        />
+
+                                        <BotoesForm
+                                            className={Style.btnSanfona}
+
+                                            texto="Remover Banner"
+                                        />
+
+                                        <BotoesForm
+                                            className={Style.btnSanfona}
+
+                                            texto="Redefinir Nome"
+                                        />
+                                        <BotoesForm
+                                            className={Style.Banir}
+                                            texto="Banir"
+                                        />
+                                    </div>
+                                )}
                             </div>
                         ))}
 
