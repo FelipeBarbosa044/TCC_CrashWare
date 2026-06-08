76008a45d1b03b1ec840f6eaef8fe7a7d566a5b6
FelipeBarbosa044 <felipebarbosaribeiro197@gmail.com>
2026-06-08 20:17:45 -0300

    Rota De verificar Adm no site

---
+++ frontend/Site/CrashWare/src/VerificacaoToken/RotaAdm.jsx
@@ -0,0 +1,48 @@
+import { useContext, useEffect, useState } from "react";
+//MUITO IMPORTANTE/ ROTA GLOBAL
+import { AuthContext } from "./AuthProvider";
+import { Navigate, useNavigate } from "react-router-dom";
+import { Api } from "../../funcoes/functions";
+import { Usuario } from "../../funcoes/user";
+
+const RotaAdm = ({ children }) => {
+
+    //Pego as informações do usuário
+    const usuario = JSON.parse(localStorage.getItem("dados"));
+
+    //Navegação --> Permite eu levar o usuario para outras telas
+    const Navegacao = useNavigate();
+
+    function VerificarAdm()
+    {
+        if (usuario == null)
+        {
+            //Levo para a pagina Inicial
+            Navegacao('/')
+        }else
+        {
+            //Pego o valor de adm do usuário
+            const adm = usuario.adm;
+
+            //Verifico se usuário é adm
+            if(adm == false)
+            {
+                //Levo para a HOME
+                Navegacao('/home')
+            }
+        }
+    }
+    
+
+    
+    //Sempre que a rota for chamada, eu verifico se a pessoa é ADM
+    useEffect(() => {
+            VerificarAdm();
+        }, []);
+          
+    
+
+    return children;
+};
+
+export { RotaAdm }
