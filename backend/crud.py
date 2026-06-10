f9232fc2e729e9519fe02c729fc37c45c0bd9f1b
FelipeBarbosa044 <felipebarbosaribeiro197@gmail.com>
2026-06-10 09:58:38 -0300

    Patente criadas no banco de dados

--- backend/crud.py
+++ backend/crud.py
@@ -29,6 +29,7 @@ session = Session()
 try:
     # session.query(Usuarios).filter(Usuarios.email == "felipewasxz@gmail.com").update({"ativo" : True})
     # session.query(Usuarios).filter(Usuarios.email == "felipebarbosaribeiro197@gmail.com").update({"ativo" : True})
+    # session.query(Patente).filter(Patente.id_patente == 3).update({"xp_minimo" : 4500})
     # session.commit()
     pass
 except Exception as exception:
