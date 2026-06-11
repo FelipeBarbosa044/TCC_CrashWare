20c7946d6d7381caa1088505c3f3fdcdf96f93b6
Gabriel Costa <costasousagabriel075@gmail.com>
2026-06-11 13:59:05 -0300

    Felipe fazer a api

--- frontend/Site/CrashWare/src/Paginas/ADM/AbasAdm/AbaRelatorios/AbaRelatorios.jsx
+++ frontend/Site/CrashWare/src/Paginas/ADM/AbasAdm/AbaRelatorios/AbaRelatorios.jsx
@@ -5,6 +5,7 @@ import { Adm } from '../../../../../funcoes/adm'
 import Style from './AbaRelatorios.module.css'
 import { PopUp } from '../../../../Componentes';
 import { useState } from 'react';
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const AbaRelatorios = () =>{
 
@@ -14,23 +15,30 @@ const AbaRelatorios = () =>{
     //Popup
     const [popup, setPopup] = useState(null);
 
-    const[total, setTotal] = useState(null)
+    //Total usuarios
+    const[total, setTotal] = useState(null);
 
-    const dadosFic = [
-        { nome: 'Aula 01', acertos: 9, tempo: 10},
-        { nome: 'Aula 02', acertos: 7, tempo: 12},
-        { nome: 'Aula 03', acertos: 8, tempo: 11},
-        { nome: 'Aula 04', acertos: 6, tempo: 15},
-        { nome: 'Aula 05', acertos: 10, tempo: 9},
-    ]
+    //Dados Gráfico
+    const [dadosGrafico, setDadosGraficos] = useState([])
+
+    //Loading dos dados
+    const [loading, setLoading] = useState(false);
+
+    // const dadosFic = [
+    //     { nome: 'Aula 01', acertos: 9, tempo: 10},
+    //     { nome: 'Aula 02', acertos: 7, tempo: 12},
+    //     { nome: 'Aula 03', acertos: 8, tempo: 11},
+    //     { nome: 'Aula 04', acertos: 6, tempo: 15},
+    //     { nome: 'Aula 05', acertos: 10, tempo: 9},
+    // ]
 
     
-    carregarInformacoes()
+    // carregarInformacoes()
 
     async function carregarInformacoes() {    
 
-        await adm.carregar_usuarios(setPopup,setTotal)
-
+        await adm.carregar_usuarios(setPopup,setTotal, setDadosGraficos)
+        // await adm.
     }
 
  
@@ -41,6 +49,7 @@ const AbaRelatorios = () =>{
     
         }, []);
     
+        // if (loading) return <p>Carregando</p>;
 
     return(
         <>
@@ -60,12 +69,27 @@ const AbaRelatorios = () =>{
                     <div className={Style.UsuariosCadastrados}>
                         <h3>Usuarios Cadastrados</h3>
                         <p>{total}</p>
+
+
+                        {/* <ResponsiveContainer width="100%" height={300}>
+                            <BarChart data={dadosGrafico}>
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="mes" />
+                                <YAxis />
+                                <Tooltip
+                                    formatter={(value, name) =>[
+                                        value, name === 'total' ? 'total acumlado' : 'Novos'
+                                    ]}
+                                    />
+                                    <Bar dataKey="total" name="Total acumulado" fill="red" />
+                            </BarChart>
+                        </ResponsiveContainer> */}
                     </div>
 
-                    <div className={Style.Acertos}>
+                    {/* <div className={Style.Acertos}>
                         <h3>Taxa de Acertos por Aula</h3>
                         <p>Em breve...</p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
